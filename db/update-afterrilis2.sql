8e5eb6b7e6ecc41e5211b171cbe3595fc03a657b
msubair <msubair@fbf9ba93-b774-6329-0ca1-4b478a20298f>
2010-08-04 17:42:01 +0000

    update database

---
+++ db/update-afterrilis2.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `lap_ssp` DROP `terbilang`;
+ALTER TABLE `wp` ADD `logo` CHAR(140) NOT NULL DEFAULT '' AFTER `klu`;
+ALTER TABLE `wp` ADD `enable_logo` TINYINT(1) NOT NULL DEFAULT 0 BEFORE `pemilik`;
